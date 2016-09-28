3a3842bcfadb73e5b6e81604e50f1685491638df
Devdatta <devdatta.godbole777@gmail.com>
2016-09-28 12:01:24 +0530

    Adds basic GET request using volley

--- app/src/main/java/dg/webserviceusepoc/MainActivity.java
+++ app/src/main/java/dg/webserviceusepoc/MainActivity.java
@@ -9,13 +9,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.ksoap2.SoapEnvelope;
-import org.ksoap2.serialization.PropertyInfo;
-import org.ksoap2.serialization.SoapObject;
-import org.ksoap2.serialization.SoapPrimitive;
-import org.ksoap2.serialization.SoapSerializationEnvelope;
-import org.ksoap2.transport.HttpTransportSE;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,7 +34,7 @@ public class MainActivity extends ActionBarActivity {
     EditText celcius;
     String getCel;
     String code = "";
-    SoapPrimitive resultString;
+   // SoapPrimitive resultString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,51 +72,64 @@ public class MainActivity extends ActionBarActivity {
         @Override
         protected void onPostExecute(Void result) {
             Log.i(TAG, "onPostExecute");
-            Toast.makeText(MainActivity.this, "Response" + resultString.toString(), Toast.LENGTH_LONG).show();
+          //  Toast.makeText(MainActivity.this, "Response" + resultString.toString(), Toast.LENGTH_LONG).show();
         }
 
     }
 
     public void calculate() {
-        String SOAP_ACTION = "http://tempuri.org/BarcodeStatus";
-        String METHOD_NAME = "BarcodeStatus";
-        String NAMESPACE = "http://tempuri.org/";
-        String URL = "http://54.149.90.101/KzWebservice/barcodescanner.asmx";
 
-        try {
-           // int abc = 4444;
-            SoapObject Request = new SoapObject(NAMESPACE, METHOD_NAME);
-            Request.addProperty("barcode", code);
-            SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-            soapEnvelope.dotNet = true;
-            soapEnvelope.setOutputSoapObject(Request);
 
-            HttpTransportSE transport = new HttpTransportSE(URL);
+        String url = "https://demo0600962.mockable.io/helloRestPostPOC";
 
-            transport.call(SOAP_ACTION, soapEnvelope);
-            resultString = (SoapPrimitive) soapEnvelope.getResponse();
-            String response = resultString.toString();
-            final Pattern acknowledgementPattern = Pattern.compile("<Acknowledgement>(.+?)</Acknowledgement>");
-            final Pattern countAckPattern = Pattern.compile("<Acknowledgement>(.+?)</Acknowledgement>");
-            final Matcher acknowledgementMatcher = acknowledgementPattern.matcher(response);
-            final Matcher countAckMatcher = acknowledgementPattern.matcher(response);
-            acknowledgementMatcher.find();
-            countAckMatcher.find();
-            Log.i(TAG, "Response caught : " + response);
-            System.out.println("Acknowledgement Token  " +acknowledgementMatcher.group(1));
-            System.out.println("CountAck Token  " +countAckMatcher.group(1));
-            if(acknowledgementMatcher.group(1) == "True" && countAckMatcher.group(1) == "True")
-            {
-                Log.d(TAG,"Valid User... User not checked in yet");
-            }else if(acknowledgementMatcher.group(1) == "True" && countAckMatcher.group(1) == "False")
-            {
-                Log.d(TAG,"Valid User... User has checked in already");
-            }else
-            {
-                Log.d(TAG,"Invalid User !!!");
+// Request a string response
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        // Result handling
+                        System.out.println(response);
+                        Log.d(TAG, response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                // Error handling
+                System.out.println("Something went wrong!");
+                Log.d(TAG, "Something went wrong!");
+                error.printStackTrace();
+
             }
-        } catch (Exception ex) {
-            Log.e(TAG, "Error: " + ex.getMessage());
-        }
+        });
+
+// Add the request to the queue
+        Volley.newRequestQueue(this).add(stringRequest);
+
+
+//            final Pattern acknowledgementPattern = Pattern.compile("<Acknowledgement>(.+?)</Acknowledgement>");
+//            final Pattern countAckPattern = Pattern.compile("<Acknowledgement>(.+?)</Acknowledgement>");
+//            final Matcher acknowledgementMatcher = acknowledgementPattern.matcher(response);
+//            final Matcher countAckMatcher = acknowledgementPattern.matcher(response);
+//            acknowledgementMatcher.find();
+//            countAckMatcher.find();
+//            Log.i(TAG, "Response caught : " + response);
+//            System.out.println("Acknowledgement Token  " +acknowledgementMatcher.group(1));
+//            System.out.println("CountAck Token  " +countAckMatcher.group(1));
+//            if(acknowledgementMatcher.group(1) == "True" && countAckMatcher.group(1) == "True")
+//            {
+//                Log.d(TAG,"Valid User... User not checked in yet");
+//            }else if(acknowledgementMatcher.group(1) == "True" && countAckMatcher.group(1) == "False")
+//            {
+//                Log.d(TAG,"Valid User... User has checked in already");
+//            }else
+//            {
+//                Log.d(TAG,"Invalid User !!!");
+//            }
+//        } catch (Exception ex) {
+//            Log.e(TAG, "Error: " + ex.getMessage());
+//        }
     }
 }
